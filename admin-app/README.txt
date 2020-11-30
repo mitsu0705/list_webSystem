4d623473afcfc4b09f8b1c783919b739ecfdd4e2
kubota <km19960705@outlook.jp>
2020-11-30 23:46:15 +0900

    社員詳細情報表示

---
+++ admin-app/README.txt
@@ -0,0 +1,16 @@
+Angularプロジェクト作成方法
+ng new [フォルダ名]
+
+ローカルサーバの立ち上げ方
+ng start
+package.jsonに記載あり
+
+AngularComponent作成方法
+ng generate component [Component名]
+以下が作成される
+.ts
+.spec.ts
+.html
+.css
+ngOnInit:ライフサイクルメソッド
+→componentが初期化されるたびに実行されるメソッド
---
+++ admin-app/src/app/member.service.ts
@@ -0,0 +1,23 @@
+import { Injectable } from '@angular/core';
+import { Member } from './member';
+import { MEMBERS } from './mock-members';
+import { Observable, of } from 'rxjs';
+import { MessageService } from './message.service';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class MemberService {
+
+  constructor(private messageService: MessageService) { }
+
+  getMembers(): Observable<Member[]> {
+    this.messageService.add('MemberService: 社員一覧データを取得しました');
+    return of(MEMBERS);
+  }
+
+  getMember(id: number): Observable<Member> {
+    this.messageService.add(`MemberService: 社員データ(id=${id})を取得しました`);
+    return of(MEMBERS.find(member => member.id === id));
+  }
+}
